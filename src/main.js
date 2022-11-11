244902ca4a7ea7524c8d32e159e23ac3a012873b
Giovanna Sousa <giovannasousa54@gmail.com>
2022-11-10 21:42:26 -0300

    oi 2

--- src/main.js
+++ src/main.js
@@ -1,36 +1,41 @@
-import './style.css';
+import "./style.css";
 
-const input = document.getElementsByTagName('input');
-const button = document.getElementsByTagName('button');
-console.log(button);
+const input = document.getElementsByTagName("input");
+const button = document.getElementById("button");
+let coins;
 
 function pegaObj(inputValor) {
-    const urlApi = `https://api.exchangerate.host/latest?base=${inputValor}`;
-    return fetch(urlApi)
-        .then((response) => response.json())
-        .then((data) => data.rates);
+  const urlApi = `https://api.exchangerate.host/latest?base=${inputValor}`;
+  return fetch(urlApi)
+    .then((response) => response.json())
+    .then((data) => {
+      coins = data.rates;
+      return data.rates;
+    });
 }
 
 function pesquisa() {
-    const moeda = input.value;
-    return pegaObj(moeda).then(console.log);
+  const moeda = input.value;
+  return pegaObj(moeda);
 }
 
-button.addEventListener('click', pesquisa);
-  
-// setTimeout(() => {
-//   
-  
-//   function valores() {
-//     const divs = document.getElementById("divs");
-//     for (let i = 0; i < value.length; i += 1) {
-//       const div = document.createElement("div");
-//       div.className = "style-divs";
-//       div.innerText = `${value[i]}: ${moeda[value[i]]}`;
-//       divs.appendChild(div);
-//     }
-//   }
-//   valores();
-// }, 1000);
+function valores(coin) {
+  const divs = document.getElementById("divs");
+  const value = Object.keys(coin);
+  for (let i = 0; i < value.length; i += 1) {
+    const div = document.createElement("div");
+    div.className = "style-divs";
+    div.innerText = `${value[i]}: ${coins[value[i]]}`;
+    divs.appendChild(div);
+  }
+}
 
-
+button.addEventListener("click", (event) => {
+  const coin = pesquisa().then((data) => data);
+  setTimeout(() => {
+    valores(coin);
+    console.log(coin);
+  }, 1000); 
+  
+  event.preventDefault();
+});
